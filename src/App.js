bb81e1d6b8b3271e18a4f711ddd3d8cf50a02fac
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-02-21 05:54:53 +0100

    Added popup with username input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header'
 import Messages from './components/Messages'
 import SendMessage from './components/SendMessage'
 import './App.css';
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [input, setInput] = useState("");
+
+ function handleChange(e) {
+   console.log(e.target.value)
+   setInput(e.target.value)
+
+   
+ }
+ function handleSubmit(e) {
+console.log("SUBMIT", e.target.value)
+setUserName(input)
+ }
   return (
     <div className="mainDiv">
-     <div className="messangerWindow">
+      <div className={userName ? "notDisplayed" : "popup" }>
+        <div className="popupContent">
+          <h1 style={{margin:"0", textAlign:"center"}}>What's your name?</h1>
+          <div>
+          <input type="text" onChange={handleChange}/>
+          <button onClick={handleSubmit}>ENTER</button>
+          </div>
+       
+        </div>
+      </div>
+     <div className={userName? "messangerWindow" : "notDisplayed"}>
        <Header />
        <Messages />
        <SendMessage />
